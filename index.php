a67ec8ee78925c84c9214f02e17f5cb49f259ae4
ToshCam <tochecamille@gmail.com>
2018-08-26 20:06:40 +0200

    add image download

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 require "vendor/autoload.php";
 
 use EventLoop\EventLoop;
+use Rx\Observable;
 use Rxnet\Redis\Redis;
 
 require "API.php";
@@ -22,20 +23,29 @@ $api->getKitties(0)
         }
         echo "{$total} Kitties\n";
 
-        return \Rx\Observable::range(1, $total)
+        return Observable::range(1, $total)
             ->flatMap(function($number) use ($api) {
                 echo "Call /kitties/$number\n";
 
                 return $api->getKitten($number)
                     ->map(function($data) {
+
+                        $image = $data['image_url'] ?? $data['image_url_cdn'];
+                        $ext = $data['is_fancy'] && $data['is_exclusive'] ? 'png' : 'svg';
+
+                        echo "Download {$data['id']}\n";
+                        $file = fopen("images/{$data['id']}.{$ext}", 'w');
+                        fwrite($file, file_get_contents($image));
+
                         return [
                             "key" => $data['id'],
-                            "value" => $data['image_url']
+                            "value" => $image
                         ];
                     })
                 ;
             })
             ->doOnNext(function(array $data) use ($redis) {
+
                 return $redis
                     ->connect('localhost:6379')
                     ->doOnNext(function() use($redis, $data) {
